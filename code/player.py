8bcca6795d5ed7c714bf29b5fe5136500bf24c14
XadasAlexGeb <hattie2908@gmail.com>
2022-02-04 23:07:10 +0100

    4th commit

--- code/player.py
+++ code/player.py
@@ -3,9 +3,10 @@ from settings import *
 
 # useless comment
 # I made some important changes to the project
+# sheesh
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups):
+	def __init__(self, pos, groups):
 		super().__init__(groups)
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft=pos)
