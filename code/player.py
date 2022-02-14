cab6b6478a5bee72fe5952d4f71ae70c9f39a4d1
lampi <Lampi890123@gmail.com>
2022-02-14 22:26:50 +0100

    input function added

--- code/player.py
+++ code/player.py
@@ -6,10 +6,27 @@ from settings import *
 class Player(pygame.sprite.Sprite):
 	def __init__(self, pos, groups):
 		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+		self.image = pygame.image.load('../graphics/NinjaAdventure/Actor/Characters/BlueNinja/Faceset.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft=pos)
 
-		self.direction = pygame.math.Vector2D()
+		self.direction = pygame.math.Vector2()
 
 	def input(self):
 		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_w]:
+			self.direction.y = -1
+		elif keys[pygame.K_s]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_a]:
+			self.direction.x = -1
+		elif keys[pygame.K_d]:
+			self.direction.x = 1
+		else:
+			self.direction.x = 0
+
+	def update(self):
+		self.input()
