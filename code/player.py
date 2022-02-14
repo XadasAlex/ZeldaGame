7b6fa1f0cc07237f343885318dc4588d17326e12
lampi <Lampi890123@gmail.com>
2022-02-14 22:45:10 +0100

    fixed diagonal speed increase

--- code/player.py
+++ code/player.py
@@ -1,37 +1,43 @@
 import pygame
 from settings import *
 
+
 # player class
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, groups):
-		super().__init__(groups)
-		self.image = pygame.image.load('../graphics/NinjaAdventure/Actor/Characters/BlueNinja/Faceset.png').convert_alpha()
-		self.rect = self.image.get_rect(topleft=pos)
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(
+            '../graphics/NinjaAdventure/Actor/Characters/BlueNinja/Faceset.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
 
-		self.direction = pygame.math.Vector2()
-		self.speed = 5
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
 
-	def input(self):
-		keys = pygame.key.get_pressed()
+    def input(self):
+        keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_w]:
-			self.direction.y = -1
-		elif keys[pygame.K_s]:
-			self.direction.y = 1
-		else:
-			self.direction.y = 0
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
 
-		if keys[pygame.K_a]:
-			self.direction.x = -1
-		elif keys[pygame.K_d]:
-			self.direction.x = 1
-		else:
-			self.direction.x = 0
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
 
-	def move(self, speed):
-		self.rect.center += self.direction * speed
+    def move(self, speed):
+        # normalizing speed so that you cant go faster diagonally
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
 
-	def update(self):
-		self.input()
-		self.move(self.speed)
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
