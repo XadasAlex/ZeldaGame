1898b11f8759078f3d879a104130e5a55635d19c
lampi <Lampi890123@gmail.com>
2022-02-15 17:25:29 +0100

    collisions added

--- code/player.py
+++ code/player.py
@@ -1,11 +1,10 @@
 import pygame
 from settings import *
 
-
 # player class
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load(
             '../graphics/NinjaAdventure/Actor/Characters/BlueNinja/Faceset.png').convert_alpha()
@@ -13,6 +12,25 @@ class Player(pygame.sprite.Sprite):
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
+
+    def collision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0: # moving right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: # moving left
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0: # moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: # moving up
+                        self.rect.top = sprite.rect.bottom
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -36,7 +54,10 @@ class Player(pygame.sprite.Sprite):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
-        self.rect.center += self.direction * speed
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
 
     def update(self):
         self.input()
